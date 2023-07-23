a351ab3768c665e52132246e9cae6372a1297f45
mramirezcc <mramirezcc@unsa.edu.pe>
2023-07-22 22:05:04 -0500

    suma&resta

--- Calculadora.java
+++ Calculadora.java
@@ -22,11 +22,11 @@ public class Calculadora {
 	}
 	
 	public int Suma(int a, int b) { 
-	
+	  return a + b;
 	}
 	
 	public int Resta(int a, int b) {
-	
+	  return a - b;
 	}
 	
 	public int Multiplicacion(int a, int b) {
