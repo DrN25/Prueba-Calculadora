7c88c847356c7c8670e6f78cc4ee276a8b0e9584
DrN25 <rafael.nina123@gmail.com>
2023-07-22 20:29:14 -0500

    Primer commit, subiendo el archivo java con el que trabajara

---
+++ Calculadora.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Calculadora {
+	
+	public int IngreseVariables1() {
+	
+	}
+	
+	public int IngreseVariables2() {
+	
+	}
+	
+	public String IngreseOperacion() {
+	
+	}
+	
+	public int Calculadora(int a, String operacion, int b) {
+	
+	}
+	
+	public int Suma(int a, int b) { 
+	
+	}
+	
+	public int Resta(int a, int b) {
+	
+	}
+	
+	public int Multiplicacion(int a, int b) {
+	
+	}
+	
+	public int Division(int a, int b) {
+	
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
