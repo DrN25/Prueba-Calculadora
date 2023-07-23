5808361ed3ea401db3cde61d6868b24308c6b536
aquispearr <aquispearr@unsa.edu.pe>
2023-07-22 21:28:45 -0500

    Elaborando los métodos IngreseVariables1, IngreseVariables2, y IngreseOperacion para obtener la entrada del usuario

--- Calculadora.java
+++ Calculadora.java
@@ -3,15 +3,18 @@ import java.util.*;
 public class Calculadora {
 	
 	public int IngreseVariables1() {
-	
+	   System.out.print("Ingrese el primer número: ");
+		 return scanner.nextInt();
 	}
 	
 	public int IngreseVariables2() {
-	
+	  System.out.print("Ingrese el segundo número: ");
+		return scanner.nextInt();
 	}
 	
 	public String IngreseOperacion() {
-	
+	  System.out.print("Ingrese la operacion (+ para suma, - para resta, * para multiplicacion y / para division ): ");
+		return scanner.next();
 	}
 	
 	public int Calculadora(int a, String operacion, int b) {
@@ -27,14 +30,11 @@ public class Calculadora {
 	}
 	
 	public int Multiplicacion(int a, int b) {
-	  return a * b;
+	
 	}
 	
 	public int Division(int a, int b) {
-	  if(b == 0){
-	    return 0;
-	  }
-	  return a / b;
+	
 	}
 	
 	public static void main(String[] args) {
